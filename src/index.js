b22c74d2a53524b940643c7b863b8dad737b3244
WinterJackson <winterjacksonwj@gmail.com>
2023-06-23 20:50:20 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -13,3 +13,87 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+// Wait for the DOM to load
+document.addEventListener('DOMContentLoaded', () => {
+  const toyCollection = document.getElementById('toy-collection');
+  const toyForm = document.querySelector('.add-toy-form');
+
+  // Function to create a card for a toy
+  const createToyCard = toy => {
+    const card = document.createElement('div');
+    card.classList.add('card');
+
+    const name = document.createElement('h2');
+    name.textContent = toy.name;
+
+    const image = document.createElement('img');
+    image.src = toy.image;
+    image.classList.add('toy-avatar');
+
+    const likes = document.createElement('p');
+    likes.id = `likes-${toy.id}`;
+    likes.textContent = `Likes: ${toy.likes}`;
+
+    const likeButton = document.createElement('button');
+    likeButton.classList.add('like-btn');
+    likeButton.id = toy.id;
+    likeButton.textContent = 'Like';
+
+    // Add event listener to the like button
+    likeButton.addEventListener('click', () => {
+      increaseToyLikes(toy);
+    });
+
+    card.appendChild(name);
+    card.appendChild(image);
+    card.appendChild(likes);
+    card.appendChild(likeButton);
+    toyCollection.appendChild(card);
+  };
+
+  // Function to increase a toy's likes
+  const increaseToyLikes = toy => {
+    const likesElement = document.querySelector(`#likes-${toy.id}`);
+    const newNumberOfLikes = toy.likes + 1;
+
+    // Send a PATCH request to update the toy's likes
+    fetch(`http://localhost:3000/toys/${toy.id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+      body: JSON.stringify({
+        likes: newNumberOfLikes,
+      }),
+    })
+      .then(response => response.json())
+      .then(data => {
+        // Update the toy's likes in the DOM
+        likesElement.textContent = `Likes: ${data.likes}`;
+      })
+  };
+
+  // Make a GET request to fetch the toy objects
+  fetch('http://localhost:3000/toys', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then(response => response.json())
+    .then(data => {
+      // Iterate over the toy objects and create cards for each
+      data.forEach(toy => {
+        createToyCard(toy);
+      });
+    })
+
+  // Handle form submission to add a new toy
+  toyForm.addEventListener('submit', event => {
+    // ...
+  });
+});
+
+
